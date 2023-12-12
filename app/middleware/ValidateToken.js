c3a8e3306c2c4ae1e097c6341101c9aa403b3209
Sfiso Nxumalo <75265642+SfisoNxumalo@users.noreply.github.com>
2023-12-12 20:00:06 +0200

    Update ValidateToken.js

--- app/middleware/ValidateToken.js
+++ app/middleware/ValidateToken.js
@@ -3,6 +3,12 @@ const config = require("../db/auth.config")
 
 const validateToken = (async (req, res, next) =>{
 
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
     let token;
     let authHeader = req.headers.Authorization || req.headers.authorization
 
